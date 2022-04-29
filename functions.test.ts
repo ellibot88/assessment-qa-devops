2a87297a55e327a0abfed5e3126984ef14fa38e8
Elliott Leonard <egleonard88@gmail.com>
2022-04-29 10:30:33 -0600

    initial build

---
+++ functions.test.ts
@@ -0,0 +1,9 @@
+const { shuffleArray } = require("./utils");
+
+describe("shuffleArray should", () => {
+  test("should be type array", () => {
+    expect(typeof shuffleArray).toBe("function");
+  });
+
+  test("output array is same length as input array", () => {});
+});
