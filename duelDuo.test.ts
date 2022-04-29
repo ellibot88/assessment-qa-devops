f7226fc138a32e0116c49a3011c014ecbaf2fc22
Elliott Leonard <egleonard88@gmail.com>
2022-04-29 12:00:40 -0600

    finished lab

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,20 +1,38 @@
+import { Builder, Capabilities, By } from "selenium-webdriver";
 
-import { Builder, Capabilities, By } from "selenium-webdriver"
+require("chromedriver");
 
-require('chromedriver')
-
-const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
-})
+  driver.get("http://localhost:3000/");
+  await driver.sleep(2000);
+});
 
 afterAll(async () => {
-    driver.quit()
-})
+  driver.quit();
+});
 
-test('Title shows up when page loads', async () => {
-    const title = await driver.findElement(By.id('title'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
-})
+test("Title shows up when page loads", async () => {
+  const title = await driver.findElement(By.id("title"));
+  const displayed = await title.isDisplayed();
+  expect(displayed).toBe(true);
+});
+
+test("Draw button displays div with id of 'choices'", async () => {
+  await driver.findElement(By.id("draw")).click();
+  await driver.sleep(2000);
+  const choices = await driver.findElement(By.id("choices"));
+  const displayed = await choices.isDisplayed();
+  expect(displayed).toBe(true);
+});
+
+test("Add to Duo button displays div with id of 'player-duo'", async () => {
+  await driver.findElement(By.id("draw")).click();
+  await driver.sleep(2000);
+  await driver.findElement(By.className("bot-btn")).click();
+  await driver.sleep(3000);
+  const choices = await driver.findElement(By.id("player-duo"));
+  const displayed = await choices.isDisplayed();
+  expect(displayed).toBe(true);
+});
--- functions.test.ts
+++ functions.test.ts
@@ -1,9 +1,14 @@
 const { shuffleArray } = require("./utils");
+const { bot } = require("./data");
 
 describe("shuffleArray should", () => {
   test("should be type array", () => {
     expect(typeof shuffleArray).toBe("function");
   });
 
-  test("output array is same length as input array", () => {});
+  test("output array is same length as input array", () => {
+    const input = bot.length;
+    const output = shuffleArray(bot).length;
+    expect(input).toEqual(output);
+  });
 });
